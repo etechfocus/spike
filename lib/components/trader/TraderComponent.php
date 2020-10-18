c6a70e8fffdb71b76bf716703cc75efa448b37af
Thomas Kwan <thomask@etechfocus.com>
2020-10-18 13:02:08 -0700

    added trader component

---
+++ lib/components/trader/TraderComponent.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once(__DIR__.'/../../engine/common/Constants.php');
+require_once(__DIR__.'/../../engine/common/EngineComponent.php');
+
+class TraderComponent extends EngineComponent {
+
+}
