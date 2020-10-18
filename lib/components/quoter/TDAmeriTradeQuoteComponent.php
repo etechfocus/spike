adaae0f82bd61f331c933c8d549e03918a4775b8
Thomas Kwan <thomask@etechfocus.com>
2020-10-18 12:33:24 -0700

    added error checking

--- lib/components/quoter/TDAmeriTradeQuoteComponent.php
+++ lib/components/quoter/TDAmeriTradeQuoteComponent.php
@@ -19,7 +19,10 @@ class TDAmeriTradeQuoteComponent extends EngineComponent {
             $params['apikey']="...";
             $this->engine->debug($uri.'?'.http_build_query($params));
         }
-        $content = file_get_contents($query);
+        $content = @file_get_contents($query);
+        if ($content === false) {
+            return false;
+        }
         $resp = json_decode($content, true);
         return $resp;
     }
@@ -28,17 +31,20 @@ class TDAmeriTradeQuoteComponent extends EngineComponent {
         $symbol = strtoupper($symbol);
         $params = array();
         $resp = $this->request('https://api.tdameritrade.com/v1/marketdata/'.$symbol.'/quotes', $params);
-        $candle = $resp[$symbol];
-        $attrs = array();
-        $attrs['date'] = date("Y-m-d",$candle['quoteTimeInLong']/1000);
-        $attrs['datetime'] = date("Y-m-d H:i:s",$candle['quoteTimeInLong']/1000);
-        $attrs['symbol'] = $symbol;
-        $attrs['open'] = $candle['openPrice'];
-        $attrs['close'] = $candle['closePrice'];
-        $attrs['bid'] = $candle['bidPrice'];
-        $attrs['ask'] = $candle['askPrice'];
-        $quote = new StockQuote($attrs);
-        return $quote;
+        $ret = false;
+        if (isset($resp[$symbol])) {
+            $candle = $resp[$symbol];
+            $attrs = array();
+            $attrs['date'] = date("Y-m-d",$candle['quoteTimeInLong']/1000);
+            $attrs['datetime'] = date("Y-m-d H:i:s",$candle['quoteTimeInLong']/1000);
+            $attrs['symbol'] = $symbol;
+            $attrs['open'] = $candle['openPrice'];
+            $attrs['close'] = $candle['closePrice'];
+            $attrs['bid'] = $candle['bidPrice'];
+            $attrs['ask'] = $candle['askPrice'];
+            $ret = new StockQuote($attrs);
+        }
+        return $ret;
     }
 
     public function getStockPriceHistory($symbol, $startDate, $endDate) {
@@ -50,22 +56,24 @@ class TDAmeriTradeQuoteComponent extends EngineComponent {
         $params['endDate']=($endDate*1000);
         $resp = $this->request('https://api.tdameritrade.com/v1/marketdata/'.$symbol.'/pricehistory', $params);
         $quotes = array();
-        foreach ($resp['candles'] as $candle) {
-            $attrs = array();
-            $attrs['date'] = date("Y-m-d",$candle['datetime']/1000);
-            if (isset($candle['quoteTimeInLong'])) {
-                $attrs['datetime'] = date("Y-m-d H:i:s",$candle['quoteTimeInLong']/1000);
-            } else {
-                $attrs['datetime'] = date("Y-m-d H:i:s",$candle['datetime']/1000);
+        if (isset($resp['candles'])) {
+            foreach ($resp['candles'] as $candle) {
+                $attrs = array();
+                $attrs['date'] = date("Y-m-d",$candle['datetime']/1000);
+                if (isset($candle['quoteTimeInLong'])) {
+                    $attrs['datetime'] = date("Y-m-d H:i:s",$candle['quoteTimeInLong']/1000);
+                } else {
+                    $attrs['datetime'] = date("Y-m-d H:i:s",$candle['datetime']/1000);
+                }
+                $attrs['symbol'] = $symbol;
+                $attrs['open'] = $candle['open'];
+                $attrs['close'] = $candle['close'];
+                $attrs['bid'] = $candle['close'];
+                $attrs['ask'] = $candle['close'];
+                $attrs['volume'] = $candle['volume'];
+                $quote = new StockQuote($attrs);
+                $quotes[] = $quote;
             }
-            $attrs['symbol'] = $symbol;
-            $attrs['open'] = $candle['open'];
-            $attrs['close'] = $candle['close'];
-            $attrs['bid'] = $candle['close'];
-            $attrs['ask'] = $candle['close'];
-            $attrs['volume'] = $candle['volume'];
-            $quote = new StockQuote($attrs);
-            $quotes[] = $quote;
         }
         return $quotes;
     }
@@ -81,46 +89,50 @@ class TDAmeriTradeQuoteComponent extends EngineComponent {
         $params['toDate']=date("Y-m-d", $endDate);
         $resp = $this->request('https://api.tdameritrade.com/v1/marketdata/chains', $params);
         $quotes = array();
-        foreach ($resp['callExpDateMap'] as $expDate => $strikes) {
-            $tokens = explode(":", $expDate);
-            $expDate = $tokens[0];
-            foreach ($strikes as $strike => $price) {
-                $attrs = array();
-                $attrs['expDate'] = $expDate;
-                $attrs['symbol'] = $price[0]['symbol'];
-                $attrs['type'] = Constants::CALL;
-                $attrs['strike'] = $strike;
-                $attrs['description'] = $price[0]['description'];
-                $attrs['bid'] = $price[0]['bid'];
-                $attrs['ask'] = $price[0]['ask'];
-                $attrs['bidSize'] = $price[0]['bidSize'];
-                $attrs['askSize'] = $price[0]['askSize'];
-                $attrs['delta'] = $price[0]['delta'];
-                $attrs['theta'] = $price[0]['theta'];
-                $attrs['vega'] = $price[0]['vega'];
-                $quote = new OptionQuote($attrs);
-                $quotes[$expDate][$strike][Constants::CALL] = $quote;
+        if (isset($resp['callExpDateMap'])) {
+            foreach ($resp['callExpDateMap'] as $expDate => $strikes) {
+                $tokens = explode(":", $expDate);
+                $expDate = $tokens[0];
+                foreach ($strikes as $strike => $price) {
+                    $attrs = array();
+                    $attrs['expDate'] = $expDate;
+                    $attrs['symbol'] = $price[0]['symbol'];
+                    $attrs['type'] = Constants::CALL;
+                    $attrs['strike'] = $strike;
+                    $attrs['description'] = $price[0]['description'];
+                    $attrs['bid'] = $price[0]['bid'];
+                    $attrs['ask'] = $price[0]['ask'];
+                    $attrs['bidSize'] = $price[0]['bidSize'];
+                    $attrs['askSize'] = $price[0]['askSize'];
+                    $attrs['delta'] = $price[0]['delta'];
+                    $attrs['theta'] = $price[0]['theta'];
+                    $attrs['vega'] = $price[0]['vega'];
+                    $quote = new OptionQuote($attrs);
+                    $quotes[$expDate][$strike][Constants::CALL] = $quote;
+                }
             }
         }
-        foreach ($resp['putExpDateMap'] as $expDate => $strikes) {
-            $tokens = explode(":", $expDate);
-            $expDate = $tokens[0];
-            foreach ($strikes as $strike => $price) {
-                $attrs = array();
-                $attrs['expDate'] = $expDate;
-                $attrs['symbol'] = $price[0]['symbol'];
-                $attrs['type'] = Constants::PUT;
-                $attrs['strike'] = $strike;
-                $attrs['description'] = $price[0]['description'];
-                $attrs['bid'] = $price[0]['bid'];
-                $attrs['ask'] = $price[0]['ask'];
-                $attrs['bidSize'] = $price[0]['bidSize'];
-                $attrs['askSize'] = $price[0]['askSize'];
-                $attrs['delta'] = $price[0]['delta'];
-                $attrs['theta'] = $price[0]['theta'];
-                $attrs['vega'] = $price[0]['vega'];
-                $quote = new OptionQuote($attrs);
-                $quotes[$expDate][$strike][Constants::PUT] = $quote;
+        if (isset($resp['putExpDateMap'])) {
+            foreach ($resp['putExpDateMap'] as $expDate => $strikes) {
+                $tokens = explode(":", $expDate);
+                $expDate = $tokens[0];
+                foreach ($strikes as $strike => $price) {
+                    $attrs = array();
+                    $attrs['expDate'] = $expDate;
+                    $attrs['symbol'] = $price[0]['symbol'];
+                    $attrs['type'] = Constants::PUT;
+                    $attrs['strike'] = $strike;
+                    $attrs['description'] = $price[0]['description'];
+                    $attrs['bid'] = $price[0]['bid'];
+                    $attrs['ask'] = $price[0]['ask'];
+                    $attrs['bidSize'] = $price[0]['bidSize'];
+                    $attrs['askSize'] = $price[0]['askSize'];
+                    $attrs['delta'] = $price[0]['delta'];
+                    $attrs['theta'] = $price[0]['theta'];
+                    $attrs['vega'] = $price[0]['vega'];
+                    $quote = new OptionQuote($attrs);
+                    $quotes[$expDate][$strike][Constants::PUT] = $quote;
+                }
             }
         }
         return $quotes;
